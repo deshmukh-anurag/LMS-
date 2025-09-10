864e83a90805ece930fd03c251955f5c1bad53a5
Anurag <anuragdeshmukh61@gmail.com>
2025-09-10 22:29:24 +0530

    Educator Footer

--- client/src/components/educator/Footer.jsx
+++ client/src/components/educator/Footer.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear()
+  
   return (
-    <div>
-      <h1>Educator Footer</h1>
-    </div>
+    <footer className="bg-white border-t py-4">
+      <div className="container mx-auto px-4">
+        <div className="flex items-center justify-between">
+          {/* Logo and Copyright */}
+          <div className="flex items-center space-x-2">
+            <img src={assets.logo} alt="Logo" className="h-8 w-auto" />
+            <span className="text-sm text-gray-600">
+              © {currentYear} All rights reserved
+            </span>
+          </div>
+          
+          {/* Social Icons */}
+          <div className="flex items-center space-x-4">
+            <a href="#" className="text-gray-600 hover:text-gray-800">
+              <img src={assets.facebook_icon} alt="Facebook" className="h-5 w-5" />
+            </a>
+            <a href="#" className="text-gray-600 hover:text-gray-800">
+              <img src={assets.twitter_icon} alt="Twitter" className="h-5 w-5" />
+            </a>
+            <a href="#" className="text-gray-600 hover:text-gray-800">
+              <img src={assets.instagram_icon} alt="Instagram" className="h-5 w-5" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
