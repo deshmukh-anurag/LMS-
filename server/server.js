7c76d3aadd88a9fb56fd84507a198ef92a58b669
Anurag <anuragdeshmukh61@gmail.com>
2025-10-03 15:12:30 +0530

    User can be added in database

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-import connectDB from './configs/mongodb.js  '
+import connectDB from './configs/mongodb.js'
 
 //Initialize express app
 const app = express();
