edaaa33bef6fbe1a7b627d352ca82abd32c47d52
Anurag <anuragdeshmukh61@gmail.com>
2025-10-03 14:14:52 +0530

    Deploy

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './configs/mongodb.js  '
+
+//Initialize express app
+const app = express();
+
+//Connect to MongoDB
+await connectDB();
+
+//Middlewares
+
+app.use(cors());
+
+//Routes
+app.get('/', (req, res) => {
+    res.send("API Working")
+})
+
+//PORT
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
