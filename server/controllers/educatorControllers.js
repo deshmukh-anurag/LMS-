0d8ab4108599c662b8c9381994d14bbfb70bf24a
Anurag <anuragdeshmukh61@gmail.com>
2025-10-05 23:47:21 +0530

    Get Educator Course API

--- server/controllers/educatorControllers.js
+++ server/controllers/educatorControllers.js
@@ -45,3 +45,16 @@ export const addCourse = async (req, res) => {
         console.log(error)
     }
 }
+
+//Get Educator Courses
+
+export const getEducatorCourses = async (req, res) => {
+  try{
+      const educator = req.auth.userId
+      const courses= await Course.find({educator  })
+      res.json({success:true , courses})
+  }
+  catch(error){
+      res.json({success:false , message:error.message})
+  }
+}
